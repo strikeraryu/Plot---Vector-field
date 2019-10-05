ab0fdf0f65d082e0da34e72f98727101f915f957
strikeraryu <striker.aryu56@gmail.com>
2019-10-05 10:47:37 +0530

    New features added - colour variation

--- Plot.py
+++ Plot.py
@@ -12,11 +12,20 @@ mag = 40
 # Thickness of vector
 thk = 15
 # Range factor of vector lenght(i.e the possible number of lenght of vector is vec_rng)
-vec_rng = 8
+vec_rng = 20
 # Pi value
 pi = 3.14159265359
+
+bg_col = (255, 255, 255)
+ln_col = (0, 0, 0)
+
+#colour variation
+col_var = False
+
 # vector image
 vector = pygame.image.load(os.path.join('images', 'vector.png'))
+# vector image with colour variation
+c_vector = [pygame.image.load(os.path.join('images', 'col_vector (1).png')), pygame.image.load(os.path.join('images', 'col_vector (2).png')), pygame.image.load(os.path.join('images', 'col_vector (3).png')), pygame.image.load(os.path.join('images', 'col_vector (4).png')), pygame.image.load(os.path.join('images', 'col_vector (5).png')), pygame.image.load(os.path.join('images', 'col_vector (6).png')), pygame.image.load(os.path.join('images', 'col_vector (7).png')), pygame.image.load(os.path.join('images', 'col_vector (8).png')), pygame.image.load(os.path.join('images', 'col_vector (9).png')), pygame.image.load(os.path.join('images', 'col_vector (10).png')), pygame.image.load(os.path.join('images', 'col_vector (11).png'))]
 win = pygame.display.set_mode((len, len))
 pygame.display.set_caption("PLOT - Vector Field")
 
@@ -40,9 +49,13 @@ def draw_vect(x, y, ang, fact):
     x = x*mag+(len)/2
     y = (-y*mag)+len/2
     # Transformation of vector lenght
-    fact = vec_rng - math.ceil(fact)
-    wth = mag-3*fact
-    tmp_vector = pygame.transform.scale(vector, (wth, thk))
+    fact = round(vec_rng - fact)
+    wth = mag-1*fact
+    if col_var:
+        tmp_vector = c_vector[int(fact/2)]
+    else:
+        tmp_vector = vector
+    tmp_vector = pygame.transform.scale(tmp_vector, (wth, thk))
     # To rotate the vector such that slope is tan(ang)
     ang = math.degrees(ang)
     tmp_vector = pygame.transform.rotate(tmp_vector, ang)
@@ -51,6 +64,7 @@ def draw_vect(x, y, ang, fact):
     win.blit(tmp_vector, rect)
 
 # To plot the graph  of vector field eq_x i + eq_y j
+
 
 def plot_graph(eq_x, eq_y, min, max):
     x = (-len/2)/mag
@@ -68,13 +82,14 @@ def plot_graph(eq_x, eq_y, min, max):
             rng = (min+max)/vec_rng
             fact = (modu(tmp_x, tmp_y)-min)/rng
             # Wrong vector at [0, 0]
-            if x!=0 or y!=0:
+            if x != 0 or y != 0:
                 draw_vect(x, y, ang, fact)
             pygame.display.update()
             y += 1
         x += 1
 
 # To calculate min and max vale of eq_x i + eq_y j
+
 
 def min_max(eq_x, eq_y):
     x = (-len/2)/mag
@@ -100,12 +115,12 @@ def min_max(eq_x, eq_y):
 
 # To draw the graph
 def draw_graph():
-    pygame.draw.rect(win, (255, 255, 255), (0, 0, len, len))
-    pygame.draw.line(win, (0, 0, 0), (0, len/2), (len, len/2), 3)
-    pygame.draw.line(win, (0, 0, 0), (len/2, 0), (len/2, len), 3)
+    pygame.draw.rect(win, bg_col, (0, 0, len, len))
+    pygame.draw.line(win, ln_col, (0, len/2), (len, len/2), 3)
+    pygame.draw.line(win, ln_col, (len/2, 0), (len/2, len), 3)
     for i in range(0, len+1, mag):
-        pygame.draw.line(win, (0, 0, 0), (i, 0), (i, len), 1)
-        pygame.draw.line(win, (0, 0, 0), (0, i), (len, i), 1)
+        pygame.draw.line(win, ln_col, (i, 0), (i, len), 1)
+        pygame.draw.line(win, ln_col, (0, i), (len, i), 1)
     pygame.display.update()
 
 
@@ -114,7 +129,7 @@ di_pole = '((x-5)/(((x-5)**2+y**2)**1.5))-((x+5)/(((x+5)**2+y**2)**1.5))i+(y/(((
 # qudrupole equation
 quad_pole = '((x-5)/(((x-5)**2+(y-5)**2)**1.5))-((x-5)/(((x-5)**2+(y+5)**2)**1.5))+((x+5)/(((x+5)**2+(y+5)**2)**1.5))-((x+5)/(((x+5)**2+(y-5)**2)**1.5))i+((y-5)/(((x-5)**2+(y-5)**2)**1.5))+((y+5)/(((x+5)**2+(y+5)**2)**1.5))-((y+5)/(((x-5)**2+(y+5)**2)**1.5))-((y-5)/(((x+5)**2+(y-5)**2)**1.5))j'
 
-# Enter you equation here 
+# Enter you equation here
 equation = di_pole
 
 
